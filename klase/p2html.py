5d1a869bb33b3bdc99749325433547964c1a8509
Luka Tubic <lukagamermaker@gmail.com>
2018-07-04 17:41:19 +0200

    komit 2

--- klase/p2html.py
+++ klase/p2html.py
@@ -2,3 +2,12 @@ class tags:
     def __init__ (self , tag , contents , tags = {"<p>" , "<h1>" , "<html>" , "<body>"}):
         self.tag = tag
         self.contents = contents
+        self.tags = tags
+
+    def ispisi():
+        if self.tag == "paragraf"
+            self.contents += tags[0]
+        if self.tag == "header"
+            self.contents += tags[1]
+        
+        
