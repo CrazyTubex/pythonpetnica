10df926e6a47c0b95d41aa188fb3cbd969041fbb
Luka Tubic <lukagamermaker@gmail.com>
2018-07-04 17:50:15 +0200

    male izmene

--- datamining/zadatak3.py
+++ datamining/zadatak3.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 
-Baza = pd.read_csv("orders.csv")
+datas = pd.read_csv("orders.csv")
 
 
 
@@ -12,7 +12,7 @@ data = len(set(Baza["order_id"]))
 dat1 = len(set(Baza["user_id"]))
 print("Prosecno: " + str(n/t))
 niz = [0] * 3 * n
-for i in Baza["user_id"]:
+for i in datas["user_id"]:
     niz[i] += 1
 data = np.array(niz)
 data1 = np.array(range(0, 3*n))
